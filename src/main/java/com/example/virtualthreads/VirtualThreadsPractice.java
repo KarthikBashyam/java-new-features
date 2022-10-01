b8779fc98cfed74cca0af89885be51549fa5df04
Karthik Bashyam <karthikb4ps@gmail.com>
2022-10-01 19:55:14 -0400

    Initial commit

---
+++ src/main/java/com/example/virtualthreads/VirtualThreadsPractice.java
@@ -0,0 +1,22 @@
+package com.example.virtualthreads;
+
+import java.util.concurrent.CountDownLatch;
+
+public class VirtualThreadsPractice {
+
+    public static void main(String[] args) throws Exception{
+
+        System.out.println("Hello world!");
+
+        var latch = new CountDownLatch(1);
+
+        Thread.startVirtualThread(() -> {
+                    System.out.println("Welcome to Virtual Threads");
+                    latch.countDown();
+                }
+        );
+
+        latch.await();
+
+    }
+}
